824a2e7383486ceae1810d44d7d67767e8fa3f60
Junaid Hashmi <hashmijunaid41@gmail.com>
2023-02-14 00:43:22 +0500

    add state on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,89 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink} from "react-router-dom";
-import { useState } from "react";
+
 //import Gal from "./Gal";
 
 function Navbar() {
  
+const [classs,setClasss]=useState("collapse navbar-collapse show");
+const [bools,setBools]=useState(false);
+
+function onclickbtn(e)
+  {
+   
+    setBools({ bools: !bools });
+    if(bools)
+    {
+      setClasss({ classs: "collapse navbar-collapse show"});
+    }
+    else{
+      setClasss({ classs: "collapse navbar-collapse" });
+    }
+    
+
+  }
   return (
     <div   >
 
-      <nav  class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark" >
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark" >
         <Link className="navbar-brand text-danger Stl"  style={{marginRight: 100}} to="/">
           Junaid7042
         </Link>
         
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarColor01"
           aria-controls="navbarColor01"
-          aria-expanded="true"
+          aria-expanded={bools}
           aria-label="Toggle navigation"
+          onClick={onclickbtn}
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
 
 
-        <div class=" collapse navbar-collapse show" id="navbarColor01">
-          <ul class=" navbar-nav mr-auto  ">
-            <li class="nav-item  px-md-2"
+        <div  className={classs} id="navbarColor01">
+          <ul className=" navbar-nav mr-auto  ">
+            <li className="nav-item  px-md-2"
             
            
             >
-              <NavLink class=" nav-link  btn " exact to="/" 
+              <NavLink className=" nav-link  btn " exact to="/" 
                
           data-toggle="collapse"
           data-target="#navbarColor01"
-          aria-expanded="true"
+          aria-expanded={bools}
               >Home </NavLink>
               
             
             </li>
-            <li class="nav-item px-md-2 ">
-            <NavLink class="nav-link btn " exact to="/about"
+            <li className="nav-item px-md-2 ">
+            <NavLink className="nav-link btn " exact to="/about"
             
           data-toggle="collapse"
           data-target="#navbarColor01"
-          aria-expanded="true"
+          aria-expanded={bools}
             >About </NavLink>
               
             </li>
-            <li class="nav-item px-md-2">
-            <NavLink class="nav-link btn"exact  to="/services"
+            <li className="nav-item px-md-2">
+            <NavLink className="nav-link btn"exact  to="/services"
             
             data-toggle="collapse"
             data-target="#navbarColor01"
-            aria-expanded="true"
+            aria-expanded={bools}
   
             >Services </NavLink>
                </li>
-               <li class="nav-item px-md-2">
-            <NavLink class="nav-link btn"exact  to="/projects"
+               <li className="nav-item px-md-2">
+            <NavLink className="nav-link btn"exact  to="/projects"
                       data-toggle="collapse"
                       data-target="#navbarColor01"
-                      aria-expanded="true"
+                      aria-expanded={bools}
             
             >Projects </NavLink>
                </li>
         
-            <li class="nav-item px-md-2">
-            <NavLink class="nav-link btn "exact  to="/contact-us"
+            <li className="nav-item px-md-2">
+            <NavLink className="nav-link btn "exact  to="/contact-us"
                       data-toggle="collapse"
                       data-target="#navbarColor01"
-                      aria-expanded="true"
+                      aria-expanded={bools}
             
             >Contact Us </NavLink>
               
             </li>
           </ul>
-          <Link class="nav-link btn "exact  to="/login">
-          <button class="btn btn-circle btn-sm btn-danger"><i className="fa fa-sign-in"
+          <Link className="nav-link btn "exact  to="/login">
+          <button className="btn btn-circle btn-sm btn-danger"><i className="fa fa-sign-in"
                     data-toggle="collapse"
                     data-target="#navbarColor01"
-                    aria-expanded="true"
+                    aria-expanded={bools}
           
           ></i> login</button>
           </Link>
