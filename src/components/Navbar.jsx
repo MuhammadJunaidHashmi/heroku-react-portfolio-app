c54efa65fec5d2956860205c1dc33d30af20b857
Junaid Hashmi <hashmijunaid41@gmail.com>
2023-02-14 01:10:37 +0500

    remove classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,96 +7,50 @@ function Navbar() {
 
   return (
     <div   >
-
-
-
-
-
       <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark" >
         <Link className="navbar-brand text-danger Stl"  style={{marginRight: 100}} to="/">
           Junaid7042
         </Link>
         
         <button
-          className="navbar-toggler collapsed"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarColor01"
           aria-controls="navbarColor01"
-          aria-expanded="false"
+          aria-expanded="true"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-
-
-        <div className=" collapse navbar-collapse " id="navbarColor01">
+        <div className=" collapse navbar-collapse show" id="navbarColor01">
           <ul className=" navbar-nav mr-auto  ">
-            <li className="nav-item  px-md-2"
-            
-           
-            >
-              <NavLink className=" nav-link  btn " exact to="/" 
-               
-          data-toggle="collapse"
-          data-target="#navbarColor01"
-          aria-expanded="false"
-              >Home </NavLink>
-              
-            
+            <li className="nav-item  px-md-2">
+
+              <NavLink className=" nav-link  btn " exact to="/" >Home </NavLink>
             </li>
             <li className="nav-item px-md-2 ">
-            <NavLink className="nav-link btn " exact to="/about"
-            
-          data-toggle="collapse"
-          data-target="#navbarColor01"
-          aria-expanded="false"
-            >About </NavLink>
+            <NavLink className="nav-link btn " exact to="/about">About </NavLink>
               
             </li>
             <li className="nav-item px-md-2">
-            <NavLink className="nav-link btn"exact  to="/services"
-            
-            data-toggle="collapse"
-            data-target="#navbarColor01"
-            aria-expanded="false"
-  
-            >Services </NavLink>
+            <NavLink className="nav-link btn"exact  to="/services">Services </NavLink>
                </li>
                <li className="nav-item px-md-2">
-            <NavLink className="nav-link btn"exact  to="/projects"
-                      data-toggle="collapse"
-                      data-target="#navbarColor01"
-                      aria-expanded="false"
-            
-            >Projects </NavLink>
+            <NavLink className="nav-link btn"exact  to="/projects">Projects </NavLink>
                </li>
         
             <li className="nav-item px-md-2">
-            <NavLink className="nav-link btn "exact  to="/contact-us"
-                      data-toggle="collapse"
-                      data-target="#navbarColor01"
-                      aria-expanded="false"
-            
-            >Contact Us </NavLink>
+            <NavLink className="nav-link btn "exact  to="/contact-us">Contact Us </NavLink>
               
             </li>
           </ul>
           <Link className="nav-link btn "exact  to="/login">
-          <button className="btn btn-circle btn-sm btn-danger"><i className="fa fa-sign-in"
-                    data-toggle="collapse"
-                    data-target="#navbarColor01"
-                    aria-expanded="false"
-          
-          ></i> login</button>
+          <button className="btn btn-circle btn-sm btn-danger"><i className="fa fa-sign-in"></i> login</button>
           </Link>
         </div>
       </nav>
-
-
-
-
     </div>
 
 
